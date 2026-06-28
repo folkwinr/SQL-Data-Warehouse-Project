2a332cb1adfdb8a4859c7d109641f6cf4ac64055
Enes <ynikenes@gmail.com>
2026-06-28 17:27:45 +0200

    Update ddl_bronze.sql

--- scripts/ddl_bronze.sql
+++ scripts/ddl_bronze.sql
@@ -3,6 +3,18 @@
 -- This procedure loads CSV files into Bronze tables
 -- ==========================================
 
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
 	-- Variables for time tracking
